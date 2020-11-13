9e6653689d3696f7c2907475630b3a4fc4e1ced3
Dhananjay Singh Vijay Singh <dv17@rice.edu>
2020-11-12 21:45:34 -0600

    pmao_kremove changes - added if statement to check if level 2

--- sys/arm64/arm64/pmap.c
+++ sys/arm64/arm64/pmap.c
@@ -1474,7 +1474,9 @@ pmap_kremove(vm_offset_t va)
 	pmap_clear(pte);
 	pmap_invalidate_page(kernel_pmap, va);
 	// Test to see level, if level = 2 and not three, call pmap_remove_kernel_l2
-	pmap_remove_kernel_l2(kernel_pmap, pte, va);
+	if (lvl == 2) {
+		pmap_remove_kernel_l2(kernel_pmap, pte, va);
+	}
 }
 
 void
